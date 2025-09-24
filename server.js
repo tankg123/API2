dcb02e3af42209948eac6943ab44b4acff28eab1
unknown <ceo@amnhacso.com>
2025-09-24 22:41:44 +0700

    datthanhs

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`✅ Server đang chạy tại https://localhost:${PORT}`);
 });
