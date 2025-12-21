1b56da4c6b1308db05c7f1e02f81bf88afbd5bec
unknown <ceo@amnhacso.com>
2025-12-21 22:25:35 +0700

    update

--- routes/qa.js
+++ routes/qa.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const qaController = require("../controller/qa");
 
-// ✅ POST trước
+// POST
 router.post("/", qaController.create);
 router.post("/ask", qaController.ask);
 
