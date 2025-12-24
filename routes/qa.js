6287fa078f8aac81f40498945288522d6af4e348
tankg123 <ceo@amnhacso.com>
2025-12-24 14:53:41 +0700

    update1

--- routes/qa.js
+++ routes/qa.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/qaController");
+const controller = require("../controller/qaController");
 
 router.get("/", controller.getAll);
 router.post("/ask", controller.ask);
